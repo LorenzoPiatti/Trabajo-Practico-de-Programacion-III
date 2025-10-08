0d703bd9c000d63796fcb032bb98463d3a2d2c80
LorenzoPiatti <lorenzopiatti2003@gmail.com>
2025-10-08 12:13:47 -0300

    funciona el carrito, falta agregar la navbar y el footer y tambien el css

--- ecommerce-apple/src/store/CartStore.js
+++ ecommerce-apple/src/store/CartStore.js
@@ -1,24 +1,27 @@
 import { create } from "zustand";
 
 export const useCartStore = create((set) => ({
-  items: [],
+  cart: [],
+
   addToCart: (product) =>
     set((state) => {
-      const exists = state.items.find((item) => item.id === product.id);
-      if (exists) {
+      const itemExists = state.cart.find((item) => item.id === product.id);
+      if (itemExists) {
         return {
-          items: state.items.map((item) =>
+          cart: state.cart.map((item) =>
             item.id === product.id
               ? { ...item, quantity: item.quantity + 1 }
               : item
           ),
         };
       }
-      return { items: [...state.items, { ...product, quantity: 1 }] };
+      return { cart: [...state.cart, { ...product, quantity: 1 }] };
     }),
+
   removeFromCart: (id) =>
     set((state) => ({
-      items: state.items.filter((item) => item.id !== id),
+      cart: state.cart.filter((item) => item.id !== id),
     })),
-  clearCart: () => set({ items: [] }),
+
+  clearCart: () => set({ cart: [] }),
 }));
