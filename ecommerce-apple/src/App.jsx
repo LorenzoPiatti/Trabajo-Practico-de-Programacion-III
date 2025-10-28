14d35ed5b4a0f8d75dedcb7e5c7c181e6b6e9ea0
gianina franquini <giani.franquini@gmail.com>
2025-10-28 15:33:29 -0300

    elimino archivo que no usamos

--- ecommerce-apple/src/App.jsx
+++ ecommerce-apple/src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -21,7 +20,6 @@ function App() {
         <Route path="/catalog/:category" element={<Catalog />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
